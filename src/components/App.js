436184efea6fc26635b05eda15dec696d865fe83
Shivani Anandan <shivanianandhan28@gmail.com>
2023-07-25 21:28:31 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+  let[sum,setSum] = useState(0)
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Sum calculator</h1>
+        <input type="number" placeholder="enter a number"
+        onChange={(e)=>{setSum(parseInt(sum)+parseInt(e.target.value))}}/>
+        <p>Sum: {sum}</p>
+
     </div>
   )
 }
